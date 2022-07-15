3b1318ae436287efb5a8b9c72665976c2025ca17
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-07-15 16:11:45 +0600

    add: dev and prod URL

--- src/contexts/socket-context.ts
+++ src/contexts/socket-context.ts
@@ -1,5 +1,7 @@
 import React from 'react';
 import { io, Socket } from 'socket.io-client'
 
-export const socket = io('localhost:4000')
+const URL = process.env.NODE_ENV === 'development' ? 'localhost:4000' : 'chat-app-back-server.herokuapp.com'
+
+export const socket = io(URL)
 export const SocketContext = React.createContext<Socket>(socket)
